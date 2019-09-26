f24a595b1be5bec8502f3026cba88938d2b2a84d
Wildhevire <wildhevire404@gmail.com>
2019-09-26 18:19:40 +0000

    finished

---
+++ src/latihan/operasilogika/Logika.java
@@ -0,0 +1,18 @@
+package latihan.operasilogika;
+
+public class Logika {
+	/*
+    Nama        : Wildan Muhammad Fikri
+    Kelas       : IF1
+    NIM         : 10118044
+    Deskripsi   : Implementasi operasi logika
+     */
+	public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+}
